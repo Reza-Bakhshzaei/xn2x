622d460326c3c3bcfaf6a90ec1a8df63a5384035
Reza Bakhsh zaei <99674430+Reza-Bakhshzaei@users.noreply.github.com>
2026-09-11 12:32:59 +0330

    best change

--- crawle/videoDownloader.py
+++ crawle/videoDownloader.py
@@ -1,8 +1,10 @@
-import m3u8_To_MP4, requests, re, os
+import requests, re, os
+from pym3u8downloader import M3U8Downloader
 
 def download_video_from_link(url):
     print(f"url= {url}")
-    if url.split("/")[-1]+".mp4" not in os.listdir():
+    name = url.split("/")[-1]+".mp4"
+    if name not in os.listdir():
         response = requests.get(url).text
         m3u8_file = response.find("html5player.setVideoHLS('")
         m = response.find("\n", m3u8_file)
@@ -11,6 +13,12 @@ def download_video_from_link(url):
         response = requests.get(f).content
         resolutions = sorted(sorted([tex for tex in response.decode("utf-8").split("\n") if re.findall("hls-(480|720|1080|360)p.*.m3u8", tex)]), key=len)
         print(f"resolutions= {resolutions}")
-        m3u8_To_MP4.multithread_download(m3u8_uri=f.replace(f.split("/")[-1], resolutions[-1]), mp4_file_name="{}.mp4".format(url.split("/")[-1]))
-        return url.split("/")[-1]+".mp4"
-    else:return url.split("/")[-1]+".mp4"
+        # m3u8_To_MP4.multithread_download(m3u8_uri=f.replace(f.split("/")[-1], resolutions[-1]), mp4_file_name="{}.mp4".format(url.split("/")[-1]))
+        link = f.replace(f.split("/")[-1], resolutions[-1])
+        print(link)
+        downloader = M3U8Downloader(
+            input_file_path=link,
+            output_file_path=name
+        )
+        return name
+    else:return name
